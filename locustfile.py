284ada0c3e2cadba65d36cd621e540f0e5853ef4
Prabhat Sharma <hi.prabhat@gmail.com>
2022-08-07 15:25:57 +0000

    index name is dynamic

--- locustfile.py
+++ locustfile.py
@@ -1,9 +1,13 @@
 import base64, json
+import random
+
 from locust import FastHttpUser, task
 
-
-
 class ZincUser(FastHttpUser):
+
+    num = random.randint(1, 1000)
+    index = "bulkv2-" + str(num)
+
 
     # @task
     # def search_basic(self):
@@ -14,25 +18,25 @@ class ZincUser(FastHttpUser):
     #         'authorization': 'Basic YWRtaW4xOlN1cGVybWFuVnNTcGlkZXJtYW4jNzM1',
     #         'content-type': 'application/json'
     #     }
-    #     self.client.post("/es/t1/_search",
+    #     self.client.post("/es/+ self.index + /_search",
     #                      json=data, headers=headers)
 
-    @task
-    def insert_bulk_data(self):
-        ''' insert_data does a basic insert in zinc using bulk api'''
-        data =  open('data/bulk.ndjson').read()
+    # @task
+    # def insert_bulk_data(self):
+    #     ''' insert_data does a basic insert in zinc using bulk api'''
+    #     data =  open('data/bulk.ndjson').read()
 
-        user = "admin"
-        password = "Complexpass#123"
-        # user = "admin"
-        # password = "admin"
-        bas64encoded_creds = base64.b64encode(bytes(user + ":" + password, "utf-8")).decode("utf-8")
+    #     user = "admin"
+    #     password = "Complexpass#123"
+    #     # user = "admin"
+    #     # password = "admin"
+    #     bas64encoded_creds = base64.b64encode(bytes(user + ":" + password, "utf-8")).decode("utf-8")
 
-        headers = {
-            'authorization': 'Basic ' + bas64encoded_creds, 
-            'content-type': 'application/json'
-        }
-        self.client.post("/api/t6v1/_bulk", data=data, headers=headers)
+    #     headers = {
+    #         'authorization': 'Basic ' + bas64encoded_creds, 
+    #         'content-type': 'application/json'
+    #     }
+    #     self.client.post("/api/+ self.index + /_bulk", data=data, headers=headers)
     
     @task
     def insert_bulkv2_data(self):
@@ -49,6 +53,7 @@ class ZincUser(FastHttpUser):
             'authorization': 'Basic ' + bas64encoded_creds, 
             'content-type': 'application/json'
         }
-        self.client.post("/api/t6v2/_bulkv2",
+
+        self.client.post("/api/"+ self.index + "/_bulkv2",
                          json=data, headers=headers)
 
