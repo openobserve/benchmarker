1e0c96aeea083886afba3c8c0d59c231e068d2cb
Prabhat Sharma <hi.prabhat@gmail.com>
2022-08-08 03:09:49 -0700

    added _multi api

--- locustfile.py
+++ locustfile.py
@@ -19,22 +19,22 @@ class ZincUser(FastHttpUser):
     #     self.client.post("/es/+ self.index + /_search",
     #                      json=data, headers=headers)
 
-    # @task
-    # def insert_bulk_data(self):
-    #     ''' insert_data does a basic insert in zinc using bulk api'''
-    #     data =  open('data/bulk.ndjson').read()
+    @task
+    def insert_bulk_data(self):
+        ''' insert_data does a basic insert in zinc using bulk api'''
+        data =  open('data/bulk.ndjson').read()
 
-    #     user = "admin"
-    #     password = "Complexpass#123"
-    #     # user = "admin"
-    #     # password = "admin"
-    #     bas64encoded_creds = base64.b64encode(bytes(user + ":" + password, "utf-8")).decode("utf-8")
+        user = "admin"
+        password = "Complexpass#123"
+        # user = "admin"
+        # password = "admin"
+        bas64encoded_creds = base64.b64encode(bytes(user + ":" + password, "utf-8")).decode("utf-8")
 
-    #     headers = {
-    #         'authorization': 'Basic ' + bas64encoded_creds, 
-    #         'content-type': 'application/json'
-    #     }
-    #     self.client.post("/api/+ self.index + /_bulk", data=data, headers=headers)
+        headers = {
+            'authorization': 'Basic ' + bas64encoded_creds, 
+            'content-type': 'application/json'
+        }
+        self.client.post("/api/+ self.index + /_bulk", data=data, headers=headers)
     
     @task
     def insert_bulkv2_data(self):
@@ -55,3 +55,21 @@ class ZincUser(FastHttpUser):
         self.client.post("/api/"+ self.index + "/_bulkv2",
                          json=data, headers=headers)
 
+    @task
+    def insert_multi_data(self):
+        ''' insert_data does a basic insert in zinc using bulkv2 api'''
+        data =  json.loads(open('data/multi.ndjson').read())
+
+        user = "admin"
+        password = "Complexpass#123"
+        # user = "admin"
+        # password = "admin"
+        bas64encoded_creds = base64.b64encode(bytes(user + ":" + password, "utf-8")).decode("utf-8")
+
+        headers = {
+            'authorization': 'Basic ' + bas64encoded_creds, 
+            'content-type': 'application/json'
+        }
+
+        self.client.post("/api/"+ self.index + "/_multi",
+                         data=data, headers=headers)
