52b79120a98a22670223431e4a6be93e8ed36779
hengfeiyang <hengfei.yang@gmail.com>
2022-10-25 10:57:49 +0800

    add locust

---
+++ app.py
@@ -0,0 +1,6 @@
+import random
+
+num = random.randint(1, 1000)
+index = "bulkv2-" + str(num)
+
+print(index)
---
+++ locustfile.py
@@ -0,0 +1,91 @@
+import base64, json
+import random
+
+from locust import FastHttpUser, task, between
+
+class ZincUser(FastHttpUser):
+    index = "perf3"
+
+    connection_timeout = 600.0
+    network_timeout = 600.0
+    wait_time = between(1, 10)
+
+    # @task
+    # def search_basic(self):
+    #     ''' search_basic does a basic search for a given query'''
+    #     data = json.loads(open('data/query_ziox.json').read())
+        
+    #     headers = {
+    #         'authorization': 'Basic YWRtaW46Q29tcGxleHBhc3MjMTIz',
+    #         'content-type': 'application/json'
+    #     }
+
+    #     self.client.post("/api/single/_search", json=data, headers=headers)
+
+    @task
+    def multi_data(self):
+        '''multi_data does a multi insert in zinc using multi api'''
+        data = open('data/ziox_multi.json').read()
+
+        user = "admin"
+        password = "Complexpass#123"
+        bas64encoded_creds = base64.b64encode(bytes(user + ":" + password, "utf-8")).decode("utf-8")
+
+        headers = {
+            'authorization': 'Basic ' + bas64encoded_creds, 
+            'content-type': 'application/json'
+        }
+        self.client.post("/api/single/k8s/_multi", data=data, headers=headers)
+
+    # @task
+    # def insert_bulk_data(self):
+    #     ''' insert_data does a basic insert in zinc using bulk api'''
+    #     data =  open('data/bulk.ndjson').read()
+
+    #     user = "admin"
+    #     password = "Complexpass#123"
+    #     # user = "admin"
+    #     # password = "admin"
+    #     bas64encoded_creds = base64.b64encode(bytes(user + ":" + password, "utf-8")).decode("utf-8")
+
+    #     headers = {
+    #         'authorization': 'Basic ' + bas64encoded_creds, 
+    #         'content-type': 'application/json'
+    #     }
+    #     # self.client.post("/api/" + self.index + "_bulk" + "/_bulk", data=data, headers=headers)
+    #     self.client.post("/api/_bulk", data=data, headers=headers)
+    
+    # @task
+    # def insert_bulkv2_data(self):
+    #     ''' insert_data does a basic insert in zinc using bulkv2 api'''
+    #     data =  json.loads(open('data/bulkv2.json').read())
+
+    #     user = "admin"
+    #     password = "Complexpass#123"
+    #     # user = "admin"
+    #     # password = "admin"
+    #     bas64encoded_creds = base64.b64encode(bytes(user + ":" + password, "utf-8")).decode("utf-8")
+
+    #     headers = {
+    #         'authorization': 'Basic ' + bas64encoded_creds, 
+    #         'content-type': 'application/json'
+    #     }
+
+    #     self.client.post("/api/"+ self.index + "_bulkv2" + "/_bulkv2", json=data, headers=headers)
+
+    # @task
+    # def insert_multi_data(self):
+    #     ''' insert_data does a basic insert in zinc using bulk api'''
+    #     data =  open('data/multi.ndjson').read()
+
+    #     user = "admin"
+    #     password = "Complexpass#123"
+    #     # user = "admin"
+    #     # password = "admin"
+    #     bas64encoded_creds = base64.b64encode(bytes(user + ":" + password, "utf-8")).decode("utf-8")
+
+    #     headers = {
+    #         'authorization': 'Basic ' + bas64encoded_creds, 
+    #         'content-type': 'application/json'
+    #     }
+    #     self.client.post("/api/" + self.index + "_multi" + "/_multi", data=data, headers=headers)
