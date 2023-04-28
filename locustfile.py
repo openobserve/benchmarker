c38d947002ee8d138be918fb3228abd9fc4e1160
hengfeiyang <hengfei.yang@gmail.com>
2023-04-28 17:45:30 +0800

    test for prometheus

--- locustfile.py
+++ locustfile.py
@@ -11,6 +11,38 @@ class ZincUser(FastHttpUser):
     network_timeout = 600.0
     # wait_time = between(1, 10)
 
+
+    ## sum by(endpoint) (rate(zo_http_incoming_requests{namespace="ziox-alpha1",organization="default"}[5m]))
+    ## sum by(endpoint) (irate(zo_http_incoming_requests{namespace="ziox-alpha1",organization="default"}[5m]))
+    ## sum by(endpoint) (increase(zo_http_incoming_requests{namespace="ziox-alpha1",organization="default"}[5m]))
+    ## sum by(stream_type) (delta(zo_storage_files{namespace="ziox-alpha1",organization="default"}[5m]))
+    ## topk(2, histogram_quantile(0.8, rate(zo_http_response_time_bucket{namespace="ziox-alpha1",organization="default"}[5m])))
+
+    @task
+    def range_query_rate(self): 
+        ''' rate '''
+        self.client.get("/api/v1/query_range?query=sum%20by(endpoint)%20(rate(zo_http_incoming_requests%7Bnamespace%3D%22ziox-alpha1%22%2Corganization%3D%22default%22%7D%5B5m%5D))&start=1682663110&end=1682664910&step=15", data="", headers={})
+
+    @task
+    def range_query_irate(self): 
+        ''' irate '''
+        self.client.get("/api/v1/query_range?query=sum%20by(endpoint)%20(irate(zo_http_incoming_requests%7Bnamespace%3D%22ziox-alpha1%22%2Corganization%3D%22default%22%7D%5B5m%5D))&start=1682663110&end=1682664910&step=15", data="", headers={})
+
+    @task
+    def range_query_increase(self): 
+        ''' increase '''
+        self.client.get("/api/v1/query_range?query=sum%20by(endpoint)%20(increase(zo_http_incoming_requests%7Bnamespace%3D%22ziox-alpha1%22%2Corganization%3D%22default%22%7D%5B5m%5D))&start=1682663110&end=1682664910&step=15", data="", headers={})
+
+    @task
+    def range_query_delta(self): 
+        ''' delta '''
+        self.client.get("/api/v1/query_range?query=sum%20by(stream_type)%20(delta(zo_storage_files%7Bnamespace%3D%22ziox-alpha1%22%2Corganization%3D%22default%22%7D%5B5m%5D))&start=1682663110&end=1682664910&step=15", data="", headers={})
+
+    @task
+    def range_query_histogram(self): 
+        ''' histogram '''
+        self.client.get("/api/v1/query_range?query=topk(2%2C%20histogram_quantile(0.8%2C%20rate(zo_http_response_time_bucket%7Bnamespace%3D%22ziox-alpha1%22%2Corganization%3D%22default%22%7D%5B5m%5D)))&start=1682663110&end=1682664910&step=15", data="", headers={})
+
     # @task
     # def search_basic(self):
     #     ''' search_basic does a basic search for a given query'''
@@ -101,20 +133,20 @@ class ZincUser(FastHttpUser):
     #     }
     #     self.client.post("/api/single/_search?sql_aggs_histogram", json=data, headers=headers)
 
-    @task
-    def multi_data(self):
-        '''multi_data does a multi insert in zinc using multi api'''
-        data = open('data/ziox_multi.json').read()
+    # @task
+    # def multi_data(self):
+    #     '''multi_data does a multi insert in zinc using multi api'''
+    #     data = open('data/ziox_multi.json').read()
 
-        user = "admin"
-        password = "Complexpass#123"
-        bas64encoded_creds = base64.b64encode(bytes(user + ":" + password, "utf-8")).decode("utf-8")
+    #     user = "admin"
+    #     password = "Complexpass#123"
+    #     bas64encoded_creds = base64.b64encode(bytes(user + ":" + password, "utf-8")).decode("utf-8")
 
-        headers = {
-            'authorization': 'Basic ' + bas64encoded_creds, 
-            'content-type': 'application/json'
-        }
-        self.client.post("/api/" + self.org_id + "/" + self.index + "/_multi", data=data, headers=headers)
+    #     headers = {
+    #         'authorization': 'Basic ' + bas64encoded_creds, 
+    #         'content-type': 'application/json'
+    #     }
+    #     self.client.post("/api/" + self.org_id + "/" + self.index + "/_multi", data=data, headers=headers)
 
     # @task
     # def insert_bulk_data(self):
