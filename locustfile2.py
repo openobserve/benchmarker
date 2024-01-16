2a2d6cfa49cfefbcb8a0b2394910405ba9d04a4c
Prabhat Sharma <hi.prabhat@gmail.com>
2024-01-16 13:16:24 -0800

    add creds

--- locustfile2.py
+++ locustfile2.py
@@ -16,14 +16,14 @@ class ZincUser(FastHttpUser):
         ''' insert_data does a basic insert in zinc using multi api'''
         data =  open('data/k8slog.ndjson').read()
 
-        user = "Wq9I9owBuYyq6Mnb7M-E"
-        password = "Me2kGXlAQ-eWZrL6ZXzjLw"
+        user = "elastic"
+        password = "YBtCaMBvFERPcUh8y8H1CGN8"
         # user = "admin"
         # password = "admin"
         bas64encoded_creds = base64.b64encode(bytes(user + ":" + password, "utf-8")).decode("utf-8")
 
         headers = {
-            'authorization': 'Apikey ' + bas64encoded_creds, 
+            'authorization': 'Basic ' + bas64encoded_creds, 
             'content-type': 'application/json'
         }
         self.client.post("/_bulk", data=data, headers=headers)
